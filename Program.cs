055dec65ed1fac0b6b540ac4d6b1500f79dd0b2e
Geoffrey Sanderson <geoffsand1984@gmail.com>
2021-03-26 15:42:27 -0400

    Create Program.cs
    Initial FizzBuzz program commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 1; counter <= 100; counter = counter + 1)
+            {
+
+            if (counter % 3 != 0 && counter % 5 != 0)
+                {
+                    Console.WriteLine(counter);
+                }
+            else if (counter % 15 == 0)
+                {
+                Console.WriteLine("FizzBuzz");
+                }
+            else if (counter % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            else if (counter % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+            }
+        }
+    }
+}
+
